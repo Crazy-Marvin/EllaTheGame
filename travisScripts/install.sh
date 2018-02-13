89ee7ca6480bbeb6a802e41645b2ad807ccb8788
REGUIG Abdelilah <reguigabdelilah@gmail.com>
2018-02-13 18:59:52 +0000

    more fixes to travis scripts

--- travisScripts/install.sh
+++ travisScripts/install.sh
@@ -11,7 +11,7 @@ if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 # By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
 # In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
 echo 'Downloading Unity 5.5.1 Windows Build Support pkg:'
-curl --retry 5 -o http://download.unity3d.com/download_unity/bfcbae508940/Windows64EditorInstaller/UnitySetup64.exe
+curl --retry 5 -o http://download.unity3d.com/download_unity/bfcbae508940/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.3.0p3.pkg
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
