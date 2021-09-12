e53f07ff7a7784262a76291def79068b7060978f
reguig-abdelilah <reguigabdelilah@gmail.com>
2021-09-12 22:29:40 +0100

    Add remaining obstacles & Add Translations

---
+++ Ella-the-game/Assets/Scripts/LanguageSelection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageSelection : MonoBehaviour
+{
+    public Dropdown languages;
+    public GameObject languagePanel;
+    int currentLanguage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        
+    }
+    public void PickLanguage()
+    {
+        currentLanguage = languages.value;
+        TranslationReader.Instance.SetCurrentLanguage(languages.value);
+    }
+    public void ToggleLanguagePanel()
+    {
+        languagePanel.SetActive(!languagePanel.activeSelf);
+        if (languagePanel.activeSelf)
+            PopulateLanguageOptions();
+    }
+    void PopulateLanguageOptions()
+    {
+        currentLanguage = PlayerPrefs.GetInt("currentLanguage");
+        languages.ClearOptions();
+        List<string> options = new List<string>();
+        for (int i = 0; i < TranslationReader.Instance.languages.Count; i++)
+        {
+            options.Add(TranslationReader.Instance.languages[i].languageCode);
+
+        }
+        languages.AddOptions(options);
+        languages.value = currentLanguage;
+    }
+}
